adc350939f7397d06282b5d5f44572bd9071ced6
lmssee <lmssee@outlool.com>
2024-06-12 07:52:32 +0800

    .

--- test/index.ts
+++ test/index.ts
@@ -1,21 +1,9 @@
 import { test, mock, describe, it } from "node:test";
-import {
-  Color,
-  fileExist,
-  get,
-  getCallerFilename,
-  getNpmPkgInfo,
-  initializeFile,
-  isWindows,
-  pathJoin,
-  runOtherCode,
-  testNpmPackageExist,
-} from "../index";
 import assert from "node:assert";
 import testReadInput from "./testReadInput";
-import testColor from './testColor';
-import testCursor from './testCursor';
-import testPath from './testPath';
+import testColor from "./testColor";
+import testCursor from "./testCursor";
+import testPath from "./testPath";
 import testNode from "./testNode";
 
 // 测试 node 相关
@@ -24,15 +12,7 @@ testNode();
 testPath();
 // 测试 Color 部分
 testColor();
-// 测试 read input 部分 
+// 测试 read input 部分
 testReadInput();
 // 测试 cursor 光标部分
 testCursor();
-
-
-/** 测试  get  */
-test.skip("test", async (t) => {
-  const result = await get("132");
-  assert.strictEqual(result, true);
-});
-
