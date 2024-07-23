fc01ec02d0b32f9d0ce2ea5889c6c73ab45c274d
lmssee <lmssee@outlool.com>
2024-07-23 13:02:14 +0800

    version: 0.0.4 么事

--- src/file.ts
+++ src/file.ts
@@ -54,12 +54,21 @@ function isExist(fileDir: string) {
  * 判断文件夹是否为空
  *
  *
+ * @returns 返回值为简单的 `-1`、`0`、`1`
+ *
+ * - `-1` 当前给出的目录名在当前目录下不是目录或是打开失败时返回该值
+ * - `0`   当前给出的目录名为目录，但是该目录为非空目录
+ * - `1` 当前目录为空
  *
  */
-function isEmpty(dirname: string): number {
-  const fileInfo = isExist(dirname);
-  if (fileInfo && fileInfo.isDirectory()) {
-    return Number(readdirSync(dirname, { withFileTypes: true }).length == 0);
+function isEmpty(dirname: string): -1 | 0 | 1 {
+  try {
+    const fileInfo = isExist(dirname);
+    if (fileInfo && fileInfo.isDirectory()) {
+      return readdirSync(dirname, { withFileTypes: true }).length == 0 ? 1 : 0;
+    }
+  } catch (error) {
+    return -1;
   }
   return -1;
 }
